f2c25fdf081c56e7a1953e23571a305ea2c1bbd7
Cristian <cristian.manuel.landeira@gmail.com>
2025-08-04 14:15:54 -0300

    Cambios

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config(); // Carga las variables del archivo .env al inicio
+
 import app from './server/src/app.js';
 
+const PORT = process.env.PORT || 3000;
 
-
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
 
 export default app;
