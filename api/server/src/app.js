abeaa8e0d3bf5faec10a5bde80d80b3836f63b40
Cristian <cristian.manuel.landeira@gmail.com>
2025-08-08 13:32:03 -0300

    Cambios

--- api/server/src/app.js
+++ api/server/src/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+dotenv.config(); // sólo para desarrollo local
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
 import usuariosRoutes from './routes/usuariosRoutes.js';
 import menuRoutes from './routes/menuRoutes.js';
 import clientesRoutes from './routes/clientesRoutes.js';
@@ -17,27 +18,22 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Configura CORS con URL explícita de frontend
+// Configura CORS con URL explícita de frontend, no '*'
 app.use(cors({
-  origin: process.env.FRONTEND_URL, // Ej: 'https://gigaflop-pro.vercel.app'
+  origin: process.env.FRONTEND_URL,  // por ejemplo: 'https://gigaflop-pro.vercel.app'
   credentials: true,
 }));
 
-app.options('*', cors({
-  origin: process.env.FRONTEND_URL,
+const allowedOrigins = [process.env.FRONTEND_URL]; // O las que necesites
+
+app.options('/*splat', cors({
+  origin: allowedOrigins,
   credentials: true,
 }));
-
 // Middleware comunes
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
-
-// Middleware para registrar las rutas solicitadas
-app.use((req, res, next) => {
-  console.log(`Ruta solicitada: ${req.method} ${req.originalUrl}`);
-  next();
-});
 
 // Rutas API
 app.use("/api/usuarios", usuariosRoutes);
@@ -45,20 +41,28 @@ app.use("/api/menu", menuRoutes);
 app.use("/api/clientes", clientesRoutes);
 app.use("/api/productos", productosRoutes);
 
-// Servir archivos estáticos desde la carpeta build de React
+// Servir archivos estáticos desde la carpeta build de React 
 app.use(express.static(path.join(__dirname, '../../client/build')));
 
 // Para cualquier ruta que no sea API, servir index.html para SPA
 app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
-
 // Middleware manejo de errores general
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Ocurrió un error en el servidor' });
 });
 
-app.use(errorHandler);
+// Middleware para registrar las rutas solicitadas (opcional) Esto lo agregue yo
+app.use((req, res, next) => {
+  console.log(`Ruta solicitada: ${req.method} ${req.originalUrl}`);
+  next();
+});
 
+app.use(errorHandler);
 export default app;
+
+/*
+
+*/
